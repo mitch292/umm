42241ada15f9261ef9155f50368c259d7f09de9d
Andrew Mitchell <andrewpmitchell7@gmail.com>
2020-05-18 08:46:23 -0400

    Handle use case where explicit time is not passed

--- cmd/time.go
+++ cmd/time.go
@@ -1,36 +1,21 @@
-/*
-Copyright © 2020 Andrew Mitchell <andrewpmitchell7@gmail.com>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
 	"fmt"
 	"time"
 
-	"github.com/spf13/viper"
-
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var timeCmd = &cobra.Command{
 	Use:   "time",
 	Short: "Convert a time zone from one to another",
 	Long: `
-	Change a time zone from one to another. Default to UTC being passed to the 
-	home time zone represented in your config file. But convert times between some of your
-	aliased time zones as well`,
+		Change a time zone from one to another. Default to UTC being passed to the 
+		home time zone represented in your config file. But convert times between some of your
+		aliased time zones as well
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		timeToConvert, _ := cmd.Flags().GetString("convert")
@@ -43,7 +28,7 @@ var timeCmd = &cobra.Command{
 			newTz:                newTz,
 		}
 
-		fmt.Println(wt.ConvertTime().Format(time.Kitchen))
+		fmt.Println(wt.convertTime().Format(time.Kitchen))
 	},
 }
 
@@ -62,7 +47,7 @@ type whatTime struct {
 	originalTimeAsString string
 }
 
-func (wt whatTime) ConvertTime() time.Time {
+func (wt whatTime) convertTime() time.Time {
 	tz, err := time.LoadLocation(wt.getTimeZone(wt.newTz))
 
 	if err != nil {
@@ -77,13 +62,18 @@ func (wt whatTime) originalTime() time.Time {
 
 	layout := "15:04:05"
 
-	newTm, err := time.ParseInLocation(layout, wt.originalTimeAsString, tz)
+	// If they don't pass us a time, lets use now
+	if len(wt.originalTimeAsString) == 0 {
+		return time.Now().In(tz)
+	}
+
+	origTime, err := time.ParseInLocation(layout, wt.originalTimeAsString, tz)
 
 	if err != nil {
 		fmt.Println("parse error:", err.Error())
 	}
 
-	return newTm
+	return origTime
 }
 
 func (wt whatTime) getTimeZone(tz string) string {
